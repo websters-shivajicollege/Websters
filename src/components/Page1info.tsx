8527bc691552727222a902d6fb3e47d271ba8aae
Abhijeetpunia0111 <abhijeetpunia01@gmail.com>
2022-12-30 16:11:52 +0530

    aah!!

---
+++ src/components/Page1info.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function Page1info() {
+  return (
+    <>
+    <div className="realtive flex w-full flex-row  font-poppins">
+        <div className="relative flex-1 mx-10 mt-3 text-white text-left">
+          <h1 className="text-3xl">hey !</h1>
+          <h1 className="text-2xl">we are Websters, a Computer science</h1>
+          <p className=" text-slate-200 leading-7 tracking-wide ">
+            Department society of the Shivaji College,<br></br>University Of
+            Delhi
+          </p>
+          <br></br>
+          <p className=" text-slate-200 leading-7 tracking-wide">
+            The Department of Computer Science was established in 1984. The
+            Department aims at upholding the cognitive aspect of education by
+            ensuring academic excelelnce and intellectual growth of its
+            students.
+          </p>
+        </div>
+        <div className="ralative ml-5 mr-7 mt-3 bg-white w-0.5"></div>
+        <div className="relative flex flex-1 mx-10 mt-3  text-white text-left">
+          <p className=" text-slate-200 leading-7 tracking-wide ">
+            The Department lays prime focus on academics intersoersed with
+            co-curricular and extra-curricular activities that bring the
+            versatility of its students to the fore and give them a sound sense
+            of perspective. The faculty comprises of experienced and dedicated
+            teachers who with their expert inputs encourage students to explore
+            new avenues. The computer society "Webstres" was started with the
+            aim to faster interest int the world of computers and technology. It
+            provides a platform for like-minded brain to communicate with each
+            other and expand their horizons.
+          </p>
+        </div>
+      </div></>
+  )
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,25 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+      fontSize: {
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+        '6xl': '4.052rem',
+        '7xl': '5.052rem',
+        '9xl': '12.052rem',
+      '12xl': '17rem',
+    },
+    extend: {
+      fontFamily:{
+        'anton' : ['Anton'],
+        'poppins' : ['Poppins'],
+      }
+    },
   },
   plugins: [],
   
